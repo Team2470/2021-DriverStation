8b088a0c53839cced13f1566030a4bd3dd049c71
Zach Thorson <zachthorson@gmail.com>
2021-02-02 09:05:33 -0600

    Experimental GUI and view/view model structure

---
+++ driver_station.py
@@ -0,0 +1,114 @@
+#/usr/bin/env python3
+
+import yaml
+from joystick import JoystickManager
+from communication import SerialBackend, BluetoothBackend
+from serial.serialutil import PortNotOpenError
+import time
+import sys
+import threading
+import structlog
+import log
+import protocol
+
+
+class DriverStation:
+
+    def __init__(self):
+        # Setup logging
+        log.setup()
+        self.logger = structlog.get_logger()
+        self.config = {}
+        self.communication_backend = None
+
+    def load_settings(self, file_path):
+        with open("config.yaml", 'r') as f:
+            self.config = yaml.load(f, Loader=yaml.FullLoader)
+
+        # Communication backend
+        communication_backend_name = self.config["communication_backend"]
+        if communication_backend_name == "serial":
+            communication_backend = SerialBackend(self.config["serial"])
+        elif communication_backend_name == "bluetooth":
+            communication_backend = BluetoothBackend(self.config["bluetooth"])
+        else:
+            self.logger.fatal("Unsupported backend", communication_backend=communication_backend_name)
+
+        # Setup the joystick manager
+        joystick_manager = JoystickManager(self.config["joystick_mapping"])
+
+    def get_settings(self):
+        return self.config
+
+    def run(self):
+        self.logger.info("Starting...")
+        running = True
+
+        def comm_read_loop():
+            while running:
+                self.communication_backend.read()
+
+        def main_loop():
+            self.logger.info("Starting main loop...")
+            while running:
+                self.joystick_manager.loop()
+
+                # Control packet
+                # TODO
+
+                # Right now just the first joystick, figure out how to properly handle joysticks later...
+                if 1 in self.joystick_manager.joysticks:
+                    joystick = self.joystick_manager.joysticks[1]
+
+                    # Build up the joystick 1 packet
+                    pkt = protocol.Joystick1Packet()
+                    pkt.axis0 = joystick.axis[0]
+                    pkt.axis1 = joystick.axis[1]
+                    pkt.axis2 = joystick.axis[2]
+                    pkt.axis3 = joystick.axis[3]
+                    pkt.axis4 = joystick.axis[4]
+                    pkt.axis5 = joystick.axis[5]
+                    # TODO lets do buttons later....
+
+                    p = pkt.pack()
+                    # logger.info("Joystick1 packet", p=p)
+                    self.communication_backend.write(p)
+
+                # Right now just the first joystick, figure out how to properly handle joysticks later...
+                if 2 in self.joystick_manager.joysticks:
+                    joystick = self.joystick_manager.joysticks[2]
+
+                    # Build up the joystick 1 packet
+                    pkt = protocol.Joystick2Packet()
+                    pkt.axis0 = joystick.axis[0]
+                    pkt.axis1 = joystick.axis[1]
+                    pkt.axis2 = joystick.axis[2]
+                    pkt.axis3 = joystick.axis[3]
+                    pkt.axis4 = joystick.axis[4]
+                    pkt.axis5 = joystick.axis[5]
+                    # TODO lets do buttons later....
+
+                    p = pkt.pack()
+                    # logger.info("Joystick2 packet", p=p)
+                    self.communication_backend.write(p)
+
+                time.sleep(0.02)
+
+        # Try to see if the serial port is open...
+        try:
+            self.communication_backend.init()
+        except PortNotOpenError as e:
+            self.logger.fatal("Unable to open serial port:" + str(e))
+            sys.exit(1)
+
+        # Start threads...
+        comm_read_thread = threading.Thread(target=comm_read_loop)
+        comm_read_thread.start()
+
+        try:
+            main_loop()  # Needs to be ran in the main loop
+        except KeyboardInterrupt:
+            running = False
+            comm_read_thread.join()
+
+        self.logger.info("Stopping...")
--- ds_gui.py
+++ ds_gui.py
@@ -6,8 +6,18 @@ from PySide6.QtCore import QObject, Slot
 from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QQmlApplicationEngine
 
+from driver_station import DriverStation
 
-class Bridge(QObject):
+
+class ViewModelMain(QObject):
+
+    def __init__(self, ds):
+        self.ds = ds
+        super().__init__()
+
+    @Slot(result=str)
+    def getConnection(self):
+        return ds.config["communication_backend"]
 
     @Slot(str, result=str)
     def getColor(self, color_name):
@@ -52,35 +62,23 @@ class Bridge(QObject):
 
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
-    view = QQuickView()
-    view.setResizeMode(QQuickView.SizeRootObjectToView)
-    view.setSource("view.qml")
-    bridge = Bridge()
-    view.engine().rootContext().setContextProperty("con", bridge)
 
-    if view.status() == QQuickView.Error:
+    ds = DriverStation()
+    ds.load_settings("config.yaml")
+    vm_main = ViewModelMain(ds)
+
+    engine = QQmlApplicationEngine()
+
+    # Expose the Python object to QML
+    context = engine.rootContext()
+    context.setContextProperty("con", vm_main)
+
+    # Get the path of the current directory, and then add the name
+    # of the QML file, to load it.
+    qmlFile = join(dirname(__file__), 'view_main.qml')
+    engine.load(abspath(qmlFile))
+
+    if not engine.rootObjects():
         sys.exit(-1)
-    view.show()
 
-    # execute and cleanup
-    app.exec_()
-    del view
-
-    # engine = QQmlApplicationEngine()
-    #
-    # # Instance of the Python object
-    # bridge = Bridge()
-    #
-    # # Expose the Python object to QML
-    # context = engine.rootContext()
-    # context.setContextProperty("con", bridge)
-    #
-    # # Get the path of the current directory, and then add the name
-    # # of the QML file, to load it.
-    # qmlFile = join(dirname(__file__), 'view.qml')
-    # engine.load(abspath(qmlFile))
-    #
-    # if not engine.rootObjects():
-    #     sys.exit(-1)
-    #
-    # sys.exit(app.exec_())
+    sys.exit(app.exec_())
